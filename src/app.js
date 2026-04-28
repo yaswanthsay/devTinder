d30244ecd479d378b808bdff94cdd874faaa37cf
Yaswanth krishna <yaswanthkrishna34@gmail.com>
2026-04-27 21:13:58 -0700

    init database

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
 const express = require("express")
 const connectDB = require("./config/database")
 const {userAuth} = require("./middlewares/auth")
+const User = require("./models/user")
 
 const app = express()
+
+
+app.post("/signup",async(req,res)=>{
+   const user = new User({
+     firstName:"Yaswanth",
+     lastName:"krishna",
+     emailId:"yaswanth@gmail.com",
+     password:"yaswanth@123"
+   })
+
+   try{
+       await user.save()
+   res.send("Signed up succesfully")
+   }catch(err){
+    console.error(err)
+   }
+
+})
 
 connectDB()
 .then(()=>{
